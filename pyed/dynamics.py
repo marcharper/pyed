6671a085db75b1fe85ebdcd6a56237d9f835ef20
Joshua <safyan@google.com>
2020-05-30 02:22:58 -0700

    refactor trajectory computation and include nesterov dynamics

--- pyed/dynamics.py
+++ pyed/dynamics.py
@@ -3,7 +3,7 @@ import warnings
 
 import numpy as np
 
-from . import incentives, geometries
+from . import incentives, geometries, information
 
 
 def normalize(x):
@@ -51,8 +51,8 @@ def fictitious_play_generator(h):
 ## Functions to actually compute trajectories
 
 
-def dynamics(state, z, incentive=None, G=None, h=1.0, mu=None, momentum=0, nesterov=False):
-    """Compute the next iteration of the dynamic."""
+def potential(state, incentive=None, G=None, mu=None):
+    """Compute the potential function for a given state"""
     if not incentive:
         incentive = incentives.DEFAULT_INCENTIVE
     if not G:
@@ -62,20 +62,29 @@ def dynamics(state, z, incentive=None, G=None, h=1.0, mu=None, momentum=0, neste
     ones = np.ones(len(state))
     g = np.dot(np.linalg.inv(G(state)), ones)
     i = incentive(state)
-    if not nesterov:
-        next_z = momentum * z + (np.dot(i, mu) - g / np.dot(g, ones) * np.sum(i))
-        next_state = state + h * next_z
-    else:
-        next_z = state + h * (np.dot(i, mu) - g / np.dot(g, ones) * np.sum(i))
-        next_state = next_z + momentum * (next_z - z)
-    # next_state = state + h * (np.dot(i, mu) - g / np.dot(g, ones) * np.sum(i))
+    return (np.dot(i, mu) - g / np.dot(g, ones) * np.sum(i))
+
+def dynamics(state, z, incentive=None, G=None, h=1.0, mu=None, momentum=0):
+    """Compute the next iteration of the dynamic."""
+    U = potential(state, incentive=incentive, G=G, mu=mu)
+    next_z = momentum * z + U
+    next_state = state + h * next_z
     return next_state, next_z
 
+def nesterov_dynamics(state, z, incentive=None, G=None, h=1.0, mu=None, momentum=0):
+    """Compute the next iteration of the dynamic with Nesterov momentum."""
+    lookahead_state = normalize(state + momentum * z)
+    U = potential(lookahead_state, incentive=incentive, G=G, mu=mu)
+    next_z = momentum * z + U
+    next_state = state + h * next_z
+    return next_state, next_z
 
 def compute_trajectory(
         initial_state, incentive, iterations=2000, h=1/200., G=None,
-        escort=None, exit_on_uniform=True, verbose=False, fitness=None,
-        project=True, mu=None, initial_z=(), momentum=None, nesterov=False):
+        escort=None, exit_on_uniform=True, exit_on_divergence_tol=False,
+        divergence_tol=.001, stable_state=None, verbose=False,
+        fitness=None, project=True, mu=None, initial_z=(),
+        momentum=None, divergence=None, nesterov=False):
     """Computes a trajectory of a dynamic until convergence or other exit
     condition is reached."""
     # Check if the time-scale is constant or not, and if it is, make it into a generator.
@@ -84,6 +93,8 @@ def compute_trajectory(
         initial_z = initial_state.copy() - np.array([1. /n] * n)
     if not momentum:
         momentum = 0
+    if not divergence:
+        divergence = information.kl_divergence
     if not inspect.isgenerator(h):
         h_gen = constant_generator(h)
     else:
@@ -98,6 +109,8 @@ def compute_trajectory(
     x = normalize(initial_state)
     z = initial_z
     t = []
+    if not stable_state:
+        stable_state = np.ones_like(initial_state) / initial_state.shape[0]
     for j, h in enumerate(h_gen):
         # Record each point for later analysis.
         t.append(x)
@@ -111,9 +124,12 @@ def compute_trajectory(
         if exit_on_uniform:
             if fitness:
                 if is_uniform(fitness(x)):
-                    break
+                    return t
             if is_uniform(incentive(x)):
-                break
+                return t
+        if exit_on_divergence_tol:
+            if divergence(stable_state, x) < divergence_tol:
+                return t
         # Are we out of the simplex?
         if not is_in_simplex(x):
             break
@@ -121,8 +137,12 @@ def compute_trajectory(
             break
         ## End Exit Conditions.
         # Iterate the dynamic.
-        x, z = dynamics(x, z, incentive=incentive, G=G, h=h, mu=mu,
-                        momentum=momentum, nesterov=nesterov)
+        if nesterov:
+          x, z = nesterov_dynamics(x, z, incentive=incentive, G=G, h=h, mu=mu,
+                        momentum=momentum)
+        else:
+          x, z = dynamics(x, z, incentive=incentive, G=G, h=h, mu=mu,
+                        momentum=momentum)
         # Check to make sure that the distribution has not left the simplex
         # due to round-off.
         # May conflict with out of simplex exit condition, but is useful for
@@ -130,11 +150,43 @@ def compute_trajectory(
         # that this is very similar to Sandholm's projection and may be
         # better handled that way.
         if project:
-            for i in range(len(x)):
-                x[i] = max(0, x[i])
+            x = np.clip(x, a_min=0, a_max=np.inf)
         # Re-normalize in case any values were rounded to 0.
         x = normalize(x)
     return t
+
+def replicator_trajectory(initial_state, fitness, iterations=2000,
+                          h=1/200., verbose=False, momentum=None, exit_on_uniform=True,
+                         exit_on_divergence_tol=True, divergence_tol=.001, nesterov=False):
+    incentive = incentives.replicator_incentive_power(fitness, 1)
+    return compute_trajectory(
+        initial_state,
+        incentive,
+        iterations=iterations,
+        h=h,
+        verbose=verbose,
+        momentum=momentum,
+        exit_on_divergence_tol=exit_on_divergence_tol,
+        divergence_tol=divergence_tol,
+        nesterov=nesterov
+    )
+
+def projection_trajectory(initial_state, fitness, iterations=2000,
+                          h=1/200., verbose=False, momentum=None,exit_on_uniform=True,
+                         exit_on_divergence_tol=True, divergence_tol=.001, nesterov=False):
+    incentive = incentives.replicator_incentive_power(fitness, 0)
+    return compute_trajectory(
+        initial_state,
+        incentive,
+        iterations=iterations,
+        escort=geometries.power_escort(0),
+        h=h,
+        verbose=verbose,
+        momentum=momentum,
+        exit_on_divergence_tol=exit_on_divergence_tol,
+        divergence_tol=divergence_tol,
+        nesterov=nesterov
+    )
 
 
 def two_population_trajectory(params, iterations=2000, verbose=False):
@@ -142,7 +194,7 @@ def two_population_trajectory(params, iterations=2000, verbose=False):
     metric, and time-scale. This function only accepts metrics G and
     generators for h."""
     t = [tuple(normalize(p[0]) for p in params)]
-    for j in range(iterations):
+    for _ in range(iterations):
         current_state = t[-1]
         h = [p[2].next() for p in params]
         i = params[0][1](current_state[1])
